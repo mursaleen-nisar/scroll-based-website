6d50fa14a55673e8a47ff5f1102fe5b85c365905
Mursaleen Nisar <mursaleennisar980@gmail.com>
2024-08-16 16:31:56 +0530

    optimised imgs 2nd time to reduce website loading time

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const context = canvas.getContext("2d");
 
 const frames = {
     currentFrame: 1,
-    totalFrames: 1614
+    totalFrames: 538
 }
 
 let imagesLoaded = 0;
@@ -12,7 +12,7 @@ const images = [];
 
 const imagePreLoader = () => {
     for(let i = 1; i <= frames.totalFrames; i++) {
-        const imageUrl = `./Frames/frame_${i.toString().padStart(4, '0')}.webp`;
+        const imageUrl = `./Frames/frame_${i.toString().padStart(3, '0')}.webp`;
         const img = new Image();
         img.src = imageUrl;
         img.onload = () => {
@@ -73,55 +73,55 @@ const startAnimation = () => {
     }
 
     tl
-        .to(frames, updateFrame(90), 'first')
+        .to(frames, updateFrame(50), 'first')
         .to('.animate1', { opacity: 1, ease: 'linear' }, 'first')
 
-        .to(frames, updateFrame(180), 'first')
+        .to(frames, updateFrame(80), 'first')
         .to('.animate1', { opacity: 0, ease: 'linear' }, 'first')
 
-        .to(frames, updateFrame(270), 'second')
+        .to(frames, updateFrame(110), 'second')
         .to('.animate2', { opacity: 1, ease: 'linear' }, 'second')
 
-        .to(frames, updateFrame(360), 'third')
+        .to(frames, updateFrame(140), 'third')
         .to('.animate2', { opacity: 1, ease: 'linear' }, 'third')
 
-        .to(frames, updateFrame(450), 'fourth')
+        .to(frames, updateFrame(170), 'fourth')
         .to('.animate2', { opacity: 0, ease: 'linear' }, 'fourth')
 
-        .to(frames, updateFrame(540), 'fifth')
+        .to(frames, updateFrame(200), 'fifth')
         .to('.animate3', { opacity: 1, ease: 'linear' }, 'fifth')
 
-        .to(frames, updateFrame(630), 'sixth')
+        .to(frames, updateFrame(230), 'sixth')
         .to('.animate3', { opacity: 1, ease: 'linear' }, 'sixth')
 
-        .to(frames, updateFrame(720), 'seventh')
+        .to(frames, updateFrame(260), 'seventh')
         .to('.animate3', { opacity: 0, ease: 'linear' }, 'seventh')
 
-        .to(frames, updateFrame(810), 'eighth')
+        .to(frames, updateFrame(290), 'eighth')
         .to('.panel', { x: '0%', ease: 'linear' }, 'eighth')
 
-        .to(frames, updateFrame(950), 'ninth')
+        .to(frames, updateFrame(320), 'ninth')
         .to('.panel', { x: '0%', ease: 'linear' }, 'ninth')
 
-        .to(frames, updateFrame(1040), 'tenth')
+        .to(frames, updateFrame(350), 'tenth')
         .to('.panel', { x: '100%', ease: 'linear' }, 'tenth')
 
-        .to(frames, updateFrame(1130), 'eleventh')
+        .to(frames, updateFrame(380), 'eleventh')
         .to(canvas, { scale: .5, borderRadius: '5rem', ease: 'linear' }, 'eleventh')
 
-        .to(frames, updateFrame(1220), 'twelveth')
+        .to(frames, updateFrame(410), 'twelveth')
         .to('.panelism', { opacity: 1, ease: 'linear' }, 'twelveth')
 
-        .to(frames, updateFrame(1310), 'thirteenth')
+        .to(frames, updateFrame(440), 'thirteenth')
         .to('.panelism span', { width: 230, ease: 'linear' }, 'thirteenth')
 
-        .to(frames, updateFrame(1400), 'fourteenth')
+        .to(frames, updateFrame(470), 'fourteenth')
         .to(canvas, { scale: 1, borderRadius: 0, ease: 'linear' }, 'fourteenth')
 
-        .to(frames, updateFrame(1500), 'fifteenth')
+        .to(frames, updateFrame(500), 'fifteenth')
         .to('.panelism', { scale: 1.7, ease: 'circ' }, 'fifteenth')
 
-        .to(frames, updateFrame(2060), 'sixteenth')
+        .to(frames, updateFrame(685), 'sixteenth')
         .to('.panelism', { scale: 1.7, ease: 'circ' }, 'sixteenth')
 
 }
